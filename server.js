d3ecfbdf5b915593f3762bf54cde47f25e209c55
Kevinoh47 <kohok47@gmail.com>
2018-09-06 14:54:51 -0700

    adding Google API client

--- server.js
+++ server.js
@@ -4,6 +4,10 @@ require ('dotenv').config();
 const express = require('express');
 const pg = require('pg');
 const superagent = require('superagent');
+let googleMapsClient = require('@google/maps').createClient({
+  key: process.env.GOOGLE_MAPS_API_KEY,
+  Promise: Promise
+});
 
 // application setup
 const app = express();
@@ -26,9 +30,27 @@ app.set('view engine', 'ejs');
 app.get('/', (request, response) => { response.render('index');});
 
 app.get('/address', (request, response) => {response.render('pages/address');});
-
+app.post('/address', getGoogleMapsData);
 // 404
 app.use('*', (request, response) => {response.render('pages/error');});
 
 // listener
 app.listen(PORT, () => console.log('listening on PORT',PORT));
+
+// Helper functions
+function getGoogleMapsData(request, response) {
+  const {address, zip, city, state} = request.body;
+  console.log('ADDRESS: ', address, 'zip: ', zip, 'city: ', city, 'state: ', state.toUpperCase());
+
+  const googAddr = `${address}, ${city}, ${state.toUpperCase()}`;
+  console.log('googAddr: ', googAddr);
+
+  googleMapsClient.geocode({address: googAddr})
+    .asPromise()
+    .then((response) => {
+      console.log(response.json.results);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
